e91d7b0e66771c6e245c52fd46977021423dbc57
TongYu <a1727692@student.adelaide.edu.au>
2020-10-03 19:35:56 +0930

    End of section 2

---
+++ API/-- SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO `Users` (Id, UserName, MyProperty)
+VALUES (1, "Bob","hu");
+
+INSERT INTO `Users` (Id, UserName, MyProperty)
+VALUES (2, "Tom", "ji");
+
+INSERT INTO `Users` (Id, UserName, MyProperty)
+VALUES (3, "Jane","jsn");
